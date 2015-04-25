3d55943ac6bbf216501133673fcee79a3a782962
vivian3795 <vivian3795@gmail.com>
2015-04-25 14:48:29 +0800

    Update test.java
    test all method and print out comments let users know the steps

--- test.java
+++ test.java
@@ -9,22 +9,35 @@
 	public static void main(String[] args){
 		//set a population
 		MyStack sample = new MyStack();
+		System.out.println("Is sample empty?  " + sample.isEmpty());
+		System.out.println("sample's toString is--"+sample.toString());
+		System.out.println("push A, B, C"); 
 		sample.push("A");
 		sample.push("B");
 		sample.push("C");
+		System.out.println("peek sample--" + sample.peek());
+		System.out.println("sample's toString is--"+sample.toString());
+		System.out.println("pop it"); 
 		sample.pop();
-		sample.peek();
+		System.out.println("sample's toString is--"+sample.toString());
 
 		//clone it
 		MyStack clone = (MyStack)((MyStack)sample).clone();
+		System.out.println("\nIs clone empty? "+ clone.isEmpty());
+		System.out.println("clone's toString is--"+clone.toString());
+		System.out.println("push A, B, C"); 
 		clone.push("A");
 		clone.push("B");
 		clone.push("C");
+		System.out.println("peek clone--" + clone.peek());
+		System.out.println("clone's toString is--"+clone.toString());
+		System.out.println("pop it"); 
 		clone.pop();
-		clone.peek();		
+		System.out.println("clone's toString is--"+clone.toString());
 		
 		//let user know the differences 
-		System.out.println(sample.getSize()+"\n"+sample.toString());
-		System.out.println(clone.getSize()+"\n"+clone.toString());
+		System.out.println("\ncompare sample and clone");		
+		System.out.println("sample's size is--"+sample.getSize());
+		System.out.println("clone's size is--"+clone.getSize());
 	}
- }
+}
